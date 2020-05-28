01d508c6f5f1b1405b07f6ac464095a351303619
Erik Post <erikpost@hotmail.com>
2020-05-29 00:19:59 +0200

    Added Main Menu with options to load Scenes; Added rotating cube to Main Menu

---
+++ Assets/Scripts/RotatingCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RotatingCube : MonoBehaviour
+{
+    [SerializeField] private float rotatingSpeed = 50.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float rotatingSpeedNormalized = rotatingSpeed * Time.deltaTime;
+      
+        Vector3 rotationPower = new Vector3(rotatingSpeedNormalized, rotatingSpeedNormalized, rotatingSpeedNormalized);
+
+        transform.Rotate(rotationPower);
+    }
+}
---
+++ Assets/Scripts/UI_MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    [SerializeField] private string MainGame = "";
+    [SerializeField] private string TestScene = "";
+
+
+    private void Start()
+    {
+        if (MainGame == "" )
+        {
+            Debug.LogError("ERROR - In UI_MainMenu script the MainGame scene is not assigned");
+        }
+        if (TestScene == "")
+        {
+            Debug.LogError("ERROR - In UI_MainMenu script the TestScene scene is not assigned");
+        }
+    }
+
+    public void buttonStartGameMain()
+    {
+        SceneManager.LoadScene(MainGame);
+    }
+
+    public void buttonOptions()
+    {
+        Debug.Log("This should open the options later!");
+    }
+
+    public void buttonTestScene()
+    {
+        SceneManager.LoadScene(TestScene);
+    }
+    public void buttonQuitGame()
+    {
+        Application.Quit();
+    }
+}
