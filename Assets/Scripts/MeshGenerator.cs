03904ccae766f0a3f3db692dec701303faaf9695
Erik Post <erikpost@hotmail.com>
2020-06-01 00:57:41 +0200

    Added start of Generating Terrain generating script

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private int xSize = 20;
+    private int zSize = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        createShape();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void createShape()
     {
-        
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        int i = 0;
+        for (int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+            }
+        }
     }
 }
