590f426be1d19e1981309f3236cce09d6745b099
Erik Post <erikpost@hotmail.com>
2020-06-01 00:51:36 +0200

    Added simple UI test

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider = null;
+    [SerializeField] private Gradient gradient = null;
+    [SerializeField] private Image fill = null;
+
+    private void Start()
+    {
+        if (slider == null) { Debug.LogError("ERROR - Slider object in HealthBar not set!"); }
+        if (gradient == null) { Debug.LogError("ERROR - Gradient object in HealthBar not set!"); }
+
+    }
+
+    public void setMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void setHealth( int health )
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
---
+++ Assets/Scripts/PlayerDamageScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageScript : MonoBehaviour
+{
+
+    private int maxHealth = 100;
+    private int currentHealth;
+    [SerializeField] private HealthBar healthBar = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (healthBar == null) { Debug.LogError("ERROR - healthBar in PlayerDamageScript is not set!"); }
+
+        currentHealth = maxHealth;
+        healthBar.setHealth(maxHealth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage(5);
+        }
+    }
+
+    private void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+    }
+}
