e127ae3819bac404b1adf9a3541b11f2e32f1235
Erik Post <erikpost@hotmail.com>
2020-06-01 23:09:29 +0200

    Added upon the base of the mesh renderer to include the mesh; challenge now is to make it a appropiate terrain generator

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,34 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityScript.Steps;
 
+[RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
-
+    // Create an object for mesh
     private Mesh mesh;
     private Vector3[] vertices;
     private int[] triangles;
 
-    private int xSize = 20;
-    private int zSize = 20;
+    [SerializeField] private int xSize = 20;
+    [SerializeField] private int zSize = 20;
 
     // Start is called before the first frame update
     void Start()
     {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
         createShape();
+        updateMesh();
     }
 
     private void createShape()
     {
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        int y = 0;
+        int quadNumber = 0;
 
-        int i = 0;
-        for (int z = 0; z <= zSize; z++)
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[i] = new Vector3(x, 0, z);
+                // Should be changed to incorperate a quad instead of positive and negative, below is close?
+                if ((x % 2 == 0) && (i % 2 == 0))
+                {
+                    y = UnityEngine.Random.Range(0, 2);
+                }
+
+                vertices[i] = new Vector3(x, y, z);
+                i++;
             }
         }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }        
     }
+
+    void updateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    if (vertices == null) { return; }
+
+    //    for (int i = 0; i < vertices.Length; i++ )
+    //    {
+    //        Gizmos.DrawSphere(vertices[i], 0.1f);
+    //    }
+    //}
 }
